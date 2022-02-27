bba69835e22bd6bc618e61c90ce8fa33820f34cf
jalvarz <java_banfield@hotmail.com>
2022-02-27 00:31:00 -0300

    .

--- ecommerce/src/App.js
+++ ecommerce/src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import './components/NavBar.js'
 
 function App() {
   return (
@@ -19,8 +18,6 @@ function App() {
           Learn React
         </a>
       </header>
-
-      <NavBar/>
     </div>
   );
 }
