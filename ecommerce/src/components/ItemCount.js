db5bf8e5258627321f31c077718912f5dc4dfa83
jalvarz <java_banfield@hotmail.com>
2022-03-13 15:29:36 -0300

    desafio clase 5

---
+++ ecommerce/src/components/ItemCount.js
@@ -0,0 +1,45 @@
+import { click } from "@testing-library/user-event/dist/click";
+
+import { useState } from "react";
+
+function ItemCount({ stock, initial }) {
+    // Desarrollar lógica
+    console.log(stock)
+    const [count,setCount] = useState(0); //se inicializa en 0
+
+    const sumar = () =>{
+        
+        setCount(count+1);
+        if (count>=stock){setCount(stock)} 
+    }
+
+    const restar = () =>{
+        setCount(count-1);
+        if (count <=0){
+            setCount(0);
+        }
+    }
+
+    const onAdd = () =>{
+        console.log("se ejecuta onAdd con ",count);
+    }
+
+    return (
+    <div>
+    <img src="assets/1.png" className="photo" alt="..." width={256}/>
+    <div className="card-body text-center">
+        <h5 className="card-title">Cofre loco</h5>
+        <p className="card-text">ejemplo de tarjetita de ecommerce. stock actual: {stock}</p>
+            <button className="btn btn-primary" onClick={restar}>-</button>
+            <label>{count}</label>
+            <button className="btn btn-primary" onClick={sumar}>+</button>
+            <br></br>
+            <button className="btn btn-primary" onClick={onAdd}>Agregar al carrito</button>
+
+    </div>
+</div>
+
+    )
+   }
+
+   export default ItemCount;
--- ecommerce/src/components/ItemListContainer.js
+++ ecommerce/src/components/ItemListContainer.js
@@ -1,9 +1,13 @@
+import ItemCount from "./ItemCount";
 function ItemListContainer()
 {
     console.log("se ejecuta ItemListContainer")
+  
     return(
         <div>
         <h3>Catalogo de productos</h3>
+        
+        <ItemCount initial={1} stock={10} > </ItemCount>
         </div>
         
     );
